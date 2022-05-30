68603c2abb0d2c672ba3bb15479c6812bde142e5
Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>
2022-05-30 16:19:26 +0530

    Revert "feat: Add `onlyOneSimpleParam` option to no-confusing-arrow rule (#15566)"
    This reverts commit 94f9b89fa6993aaf165f54315d9cea530d23d91f.

--- tests/lib/rules/no-confusing-arrow.js
+++ tests/lib/rules/no-confusing-arrow.js
@@ -30,15 +30,7 @@ ruleTester.run("no-confusing-arrow", rule, {
         { code: "var x = (a) => { return 1 ? 2 : 3; }", options: [{ allowParens: false }] },
 
         "var x = a => (1 ? 2 : 3)",
-        { code: "var x = a => (1 ? 2 : 3)", options: [{ allowParens: true }] },
-
-        "var x = (a,b) => (1 ? 2 : 3)",
-        { code: "() => 1 ? 2 : 3", options: [{ onlyOneSimpleParam: true }] },
-        { code: "(a, b) => 1 ? 2 : 3", options: [{ onlyOneSimpleParam: true }] },
-        { code: "(a = b) => 1 ? 2 : 3", options: [{ onlyOneSimpleParam: true }] },
-        { code: "({ a }) => 1 ? 2 : 3", options: [{ onlyOneSimpleParam: true }] },
-        { code: "([a]) => 1 ? 2 : 3", options: [{ onlyOneSimpleParam: true }] },
-        { code: "(...a) => 1 ? 2 : 3", options: [{ onlyOneSimpleParam: true }] }
+        { code: "var x = a => (1 ? 2 : 3)", options: [{ allowParens: true }] }
     ],
     invalid: [
         {
@@ -78,48 +70,6 @@ ruleTester.run("no-confusing-arrow", rule, {
         {
             code: "var x = (a) => 1 ? 2 : 3",
             output: "var x = (a) => (1 ? 2 : 3)",
-            errors: [{ messageId: "confusing" }]
-        },
-        {
-            code: "var x = () => 1 ? 2 : 3",
-            output: "var x = () => (1 ? 2 : 3)",
-            errors: [{ messageId: "confusing" }]
-        },
-        {
-            code: "var x = () => 1 ? 2 : 3",
-            output: "var x = () => (1 ? 2 : 3)",
-            options: [{}],
-            errors: [{ messageId: "confusing" }]
-        },
-        {
-            code: "var x = () => 1 ? 2 : 3",
-            output: "var x = () => (1 ? 2 : 3)",
-            options: [{ onlyOneSimpleParam: false }],
-            errors: [{ messageId: "confusing" }]
-        },
-        {
-            code: "var x = (a, b) => 1 ? 2 : 3",
-            output: "var x = (a, b) => (1 ? 2 : 3)",
-            errors: [{ messageId: "confusing" }]
-        },
-        {
-            code: "var x = (a = b) => 1 ? 2 : 3",
-            output: "var x = (a = b) => (1 ? 2 : 3)",
-            errors: [{ messageId: "confusing" }]
-        },
-        {
-            code: "var x = ({ a }) => 1 ? 2 : 3",
-            output: "var x = ({ a }) => (1 ? 2 : 3)",
-            errors: [{ messageId: "confusing" }]
-        },
-        {
-            code: "var x = ([a]) => 1 ? 2 : 3",
-            output: "var x = ([a]) => (1 ? 2 : 3)",
-            errors: [{ messageId: "confusing" }]
-        },
-        {
-            code: "var x = (...a) => 1 ? 2 : 3",
-            output: "var x = (...a) => (1 ? 2 : 3)",
             errors: [{ messageId: "confusing" }]
         }
     ]
